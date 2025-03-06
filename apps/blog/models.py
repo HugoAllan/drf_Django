831128437b157937938427d597fc136657f27834
Alan <martinezalan0812@gmail.com>
2025-03-06 16:54:29 -0600

    tercer commit, tercer dia

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class Post(models
+class Post(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=256)
+    content = models.TextField()
+    thumbnail = models.ImageField(upload_to='thumbnails/')
+    created_at = models.DateTimeField(auto_now_add=True)
--- core/settings.py
+++ core/settings.py
@@ -125,6 +125,9 @@ USE_TZ = True
 STATIC_LOCATION = 'static'
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -142,7 +145,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [env("REDIS_URL")],
+            'hosts': [env("REDIS_URL")]
         }
     }
 }
